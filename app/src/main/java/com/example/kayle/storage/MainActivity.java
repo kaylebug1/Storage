9240442fe30d98beddb0aebd12f840d6f982bcaf
Kayle Sillito <kaylebug1@yahoo.com>
2016-03-02 08:40:40 -0700

    Starts at 0

--- app/src/main/java/com/example/kayle/storage/MainActivity.java
+++ app/src/main/java/com/example/kayle/storage/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        System.out.println("I LIKE FOOD");
+        TextView t = (TextView) findViewById(R.id.textBox);
+        t.setText("0");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
